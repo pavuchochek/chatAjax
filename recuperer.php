2e5cec3a16f5c0fd4a37e5a7526c9f9bec116aec
username-Everam <simsimi.armand@gmail.com>
2024-03-12 10:33:08 +0100

    manque enregistrer.php, la db et l'exo 5

---
+++ recuperer.php
@@ -0,0 +1,35 @@
+<?php
+    // Création de la connexion
+    $conn = new mysqli();
+
+    // Vérification de la connexion
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données : " . $conn->connect_error);
+    }
+
+    // Requête pour récupérer les messages de la base de données
+    $sql = "SELECT * FROM chatJS ORDER BY horaire DESC LIMIT 10";
+    $result = $conn->query($sql);
+
+    // Vérification s'il y a des résultats
+    if ($result->num_rows > 0) {
+        // Construction du HTML des messages
+        while($row = $result->fetch_assoc()) {
+            $id = $row["id"];
+            $message = htmlspecialchars($row["contenu"]);
+            $pseudo = htmlspecialchars($row["userPseudo"]);
+            $date = date("H:i:s", $row["horaire"]);
+
+            // Formatage du message en HTML
+            echo "<div class='message'>
+                    <span class='message'$id>$message</span>
+                    <span class='info_message'>$pseudo | $date</span>
+                </div>";
+        }
+    } else {
+        echo "Aucun message trouvé.";
+    }
+
+    // Fermeture de la connexion
+    $conn->close();
+?>
