27a3dbfbc55cb19c8b309f2dfc9b39a0478af5b8
username-Everam <simsimi.armand@gmail.com>
2024-03-20 14:49:31 +0100

    VERSION STABLE

--- src/enregistrer.php
+++ src/enregistrer.php
@@ -1,17 +1,21 @@
 <?php
-
-
     $pseudo = $_POST['pseudo'];
     $contenu = $_POST['message'];
+    if ($pseudo == "" || $contenu == "") {
+        die("Erreur : pseudo ou message vide");
+    }
     enregistrer($pseudo, $contenu);
 
     function enregistrer($pseudo, $contenu) {
         require_once "Connexion.php";
         // Création de la connexion
         $conn = Connexion::getInstance();
+        
         // Requête SQL
-        $sql = "INSERT INTO chatJS (userPseudo, contenu) VALUES ('$pseudo', '$contenu')";
-        // Exécution de la requête
-        $conn->exec($sql);
+        $sql = "INSERT INTO chatJS (userPseudo, contenu) VALUES (:pseudo, :contenu)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':pseudo', $pseudo);
+        $stmt->bindParam(':contenu', $contenu);
+        $stmt->execute();
     }
 ?>
--- src/recuperer.php
+++ src/recuperer.php
@@ -2,24 +2,20 @@
 require_once "Connexion.php";
 // Création de la connexion
 $conn = Connexion::getInstance();
+// Requête SQL
+$sql = "SELECT * FROM chatJS";
 
-    $debut = $_GET['debut'];
+// Exécution de la requête
+$result = $conn->query($sql);
 
+// Récupération des résultats
+$messages = $result->fetchAll(PDO::FETCH_ASSOC);
 
-    // Requête SQL
-    $sql = "SELECT * FROM chatJS";
-
-    // Exécution de la requête
-    $result = $conn->query($sql);
-
-    // Récupération des résultats
-    $messages = $result->fetchAll(PDO::FETCH_ASSOC);
-
-    // Affichage des résultats
-    foreach ($messages as $message) {
-        $contenu = $message['contenu'];
-        $pseudo = $message['userPseudo'];
-        $date = $message['horaire'];
-        echo "<p><strong>$pseudo</strong> : $contenu <em>($date)</em></p>";
-    }
+// Affichage des résultats
+foreach ($messages as $message) {
+    $contenu = $message['contenu'];
+    $pseudo = $message['userPseudo'];
+    $date = $message['horaire'];
+    echo "<p><strong>$pseudo</strong> : $contenu <em>($date)</em></p><hr>";
+}
 ?>
