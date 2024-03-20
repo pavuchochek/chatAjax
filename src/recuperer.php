acbe0b48d78f4b85f2c5705e72a90f990a345e68
username-Everam <simsimi.armand@gmail.com>
2024-03-20 15:22:41 +0100

    Truc qui s'approche d'un truc qui marche mais ça marche toujours pas

--- src/recuperer.php
+++ src/recuperer.php
@@ -1,21 +1,56 @@
 <?php
 require_once "Connexion.php";
-// Création de la connexion
-$conn = Connexion::getInstance();
-// Requête SQL
-$sql = "SELECT * FROM chatJS";
+if (isset($_GET['debut'])) {
+    recuperationid();
+} else {
+    $liste_id = $_GET['liste_id'];
+    recuperationdonnee($liste_id[0]);
+}
 
-// Exécution de la requête
-$result = $conn->query($sql);
+function recuperationid() {
+    // Création de la connexion
+    $conn = Connexion::getInstance();
 
-// Récupération des résultats
-$messages = $result->fetchAll(PDO::FETCH_ASSOC);
+    // Requête SQL
+    $sql = "SELECT * FROM chatJS ORDER BY horaire DESC";
+    // Exécution de la requête
+    $result = $conn->query($sql);
 
-// Affichage des résultats
-foreach ($messages as $message) {
-    $contenu = $message['contenu'];
-    $pseudo = $message['userPseudo'];
-    $date = $message['horaire'];
-    echo "<p><strong>$pseudo</strong> : $contenu <em>($date)</em></p><hr>";
+    // Récupération des résultats
+    $messages = $result->fetchAll(PDO::FETCH_ASSOC);
+    $liste_id = [];
+    // Affichage des résultats
+    foreach ($messages as $message) {
+        $contenu = $message['contenu'];
+        $pseudo = $message['userPseudo'];
+        $date = $message['horaire'];
+        $id = $message['idMessage'];
+        $liste_id[] = $id;
+        //echo "<p><strong>$pseudo</strong> : $contenu <em>($date)</em></p><hr>";
+    }
+    foreach ($liste_id as $id) {
+        echo $id . " ";
+    }
+}
+
+function recuperationdonnee(int $id) {
+    // Création de la connexion
+    $conn = Connexion::getInstance();
+
+    // Requête SQL
+    $sql = "SELECT * FROM chatJS WHERE idMessage >= $id ORDER BY horaire DESC";
+    // Exécution de la requête
+    $result = $conn->query($sql);
+
+    // Récupération des résultats
+    $messages = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    // Affichage des résultats
+    foreach ($messages as $message) {
+        $contenu = $message['contenu'];
+        $pseudo = $message['userPseudo'];
+        $date = $message['horaire'];
+        echo "<p><strong>$pseudo</strong> : $contenu <em>($date)</em></p><hr>";
+    }
 }
 ?>
