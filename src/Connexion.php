e8e10f8df5d2c0c1ce08bf8b3dc726214a5e3211
Sofia Gribanova <grisoffa@gmail.com>
2024-04-02 10:44:46 +0200

    Update Connexion.php

--- src/Connexion.php
+++ src/Connexion.php
@@ -4,12 +4,18 @@
 class Connexion{
     public static Connexion | null $_instance = null;
     private PDO $_pdo;
+   
 
     private function __construct () {
+        $env = parse_ini_file(__DIR__ . '/../.env');
+        $db_address=$env['DATABASE_ADDRESS'];
+        $db_user=$env['USER'];
+        $db_pass=$env['PASSWORD'];
+        $db_name=$env['DATABASE_NAME'];
         try {
             $base_url = "mysql:host=%s;dbname=%s;charset=utf8";
-            $url = sprintf($base_url, 'chatDb', 'chat');
-            $this->_pdo = new PDO($url, 'test', 'test');
+            $url = sprintf($base_url, $db_address, $db_name);
+            $this->_pdo = new PDO($url, $db_user, $db_pass);
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
